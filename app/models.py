a36be1d285ecb86dba337faee3151309cf4824fc
ghogharimeet21 <ghogharimeet2121@gmail.com>
2024-05-06 12:57:56 +0530

    added more fields in model

--- app/models.py
+++ app/models.py
@@ -5,3 +5,6 @@ from django.db import models
 
 class JobPost(models.Model):
     title = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    date = models.DateTimeField(auto_now_add=True)
+    salary = models.IntegerField()
