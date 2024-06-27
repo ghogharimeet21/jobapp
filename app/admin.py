c3246919b2e7ca34f8f3d65100592b20a25d4024
Meet <161622011+ghogharimeet21@users.noreply.github.com>
2024-06-27 16:37:00 +0530

    added search feture in admin panal

--- app/admin.py
+++ app/admin.py
@@ -6,5 +6,7 @@ class JobAdmin(admin.ModelAdmin):
 
     list_filter = ("date","salary","expiry",)
 
+    search_fields = ("title","description",)
+    search_help_text = "write in your query and hit enter"
 # Register your models here.
 admin.site.register(JobPost, JobAdmin)
