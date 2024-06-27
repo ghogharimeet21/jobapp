6bc9f3a45399b2db02dc1aa83287d45135cce2ba
Meet <161622011+ghogharimeet21@users.noreply.github.com>
2024-06-27 16:29:03 +0530

    added filter in admin pannal for more customization in admin panal

--- app/admin.py
+++ app/admin.py
@@ -4,5 +4,7 @@ from app.models import JobPost
 class JobAdmin(admin.ModelAdmin):
     list_display = ("__str__","title","salary","date",)
 
+    list_filter = ("date","salary","expiry",)
+
 # Register your models here.
 admin.site.register(JobPost, JobAdmin)
